e26488f984f9e4d4283f12e1161e2c95c9570415
Chinna Subramaniam <chinna.subramaniam@sfgov.org>
2021-02-03 19:59:09 -0800

    Operators

--- coderdojo/src/corejava/Main.java
+++ coderdojo/src/corejava/Main.java
@@ -1,9 +1,10 @@
 package corejava;
 
-public class Operators {
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 System.out.println("Hello");
 	}
 
